76bff70d8b32c850712080884470e1f3c9abdb0f
Quotennial <yusufsohoye@googlemail.com>
2021-06-28 23:32:02 +0100

    shef link

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
   <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center justify-content-between">
 
-      <h1 class="logo"><a href="index.html"><img src="assets/img/logo.jpeg"/></a></h1>
+      <h1 class="logo"><a href="https://www.sheffield.ac.uk/economics"><img src="assets/img/logo.jpeg"/></a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
