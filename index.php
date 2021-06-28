2f40d32406bb779176934f94959fbeed2b4e39f1
Quotennial <yusufsohoye@googlemail.com>
2021-06-28 23:14:08 +0100

    git push

--- index.php
+++ index.php
@@ -219,10 +219,10 @@
               <div class="phone">
                 <i class="bi bi-phone"></i>
                 <h4>Call:</h4>
-                <p>+0114 222 5151</p>
+                <p>+ 0114 222 5151</p>
               </div>
               
-              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13613.293980442373!2d74.32354245!3d31.460286199999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1624361784898!5m2!1sen!2s" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+              <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13613.293980442373!2d74.32354245!3d31.460286199999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1624361784898!5m2!1sen!2s" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe> -->
             </div>
 
           </div>
